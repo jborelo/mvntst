607409e2a7505ffaa83e8affe65d751fa73203b7
Jarek <jborelo@gmail.com>
2015-12-04 01:05:18 +0100

    fat jar: uber jar

--- src/main/java/unv/jbo/App.java
+++ src/main/java/unv/jbo/App.java
@@ -1,13 +1,25 @@
 package unv.jbo;
-
+import org.apache.commons.lang.StringUtils;
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+
+
+public static void main(String[] args) {
+		sayHello();
+		sayHelloAgain();
+	}
+
+	public static void sayHello() {
+		System.out.println("MyTest says hello!");
+	}
+
+	public static void sayHelloAgain() {
+		System.out.println(StringUtils.swapCase("MyTest says hello again!"));
+	}
+
+
 }
